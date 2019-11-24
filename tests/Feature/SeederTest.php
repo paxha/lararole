3025bc00456a527ffc98557575ddbd86044efa5b
paxha <xpaxha@gmail.com>
2019-11-24 15:38:10 +0500

    seeder class comment and code copied in seeder test class

--- tests/Feature/SeederTest.php
+++ tests/Feature/SeederTest.php
@@ -17,7 +17,27 @@ class SeederTest extends TestCase
 
     public function testCase()
     {
-        $this->artisan('db:seed', ['--class' => LararoleSeeder::class]);
+//        $this->artisan('db:seed', ['--class' => LararoleSeeder::class]);
+
+        foreach (config('lararole.modules') as $module) {
+            $m = Module::create([
+                'name' => $module['name'],
+                'icon' => @$module['icon']
+            ]);
+
+            if (@$module['modules']) {
+                $m->create_modules(@$module['modules']);
+            }
+        }
+
+        $role = Role::create(['name' => 'Super Admin']);
+        $role->modules()->attach(Module::isRoot()->get()->pluck('id'), ['permission' => 'write']);
+
+        factory(Role::class, 10)->create();
+
+        Role::where('slug', '!=', 'super_admin')->get()->each(function ($role) {
+            $role->modules()->attach(Module::all()->random(rand(1, 5))->pluck('id')->toArray());
+        });
 
         factory(User::class, 10)->create();
 
