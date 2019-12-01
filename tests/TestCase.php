ee8f73ce6c06a3ff35a399bf10fcb4c2ffb7ba32
paxha <xpaxha@gmail.com>
2019-12-01 11:14:50 +0500

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,11 +17,11 @@ class TestCase extends \Orchestra\Testbench\TestCase
     {
         parent::setUp();
 
-        $this->loadMigrationsFrom(__DIR__.'/../src/database/migrations');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../src/database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
-        $this->withFactories(__DIR__.'/../src/database/factories');
-        $this->withFactories(__DIR__.'/database/factories');
+        $this->withFactories(__DIR__ . '/../src/database/factories');
+        $this->withFactories(__DIR__ . '/database/factories');
 
         $tables = DB::connection()->getDoctrineSchemaManager()->listTableNames();
 
@@ -43,11 +43,11 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('database.default', 'test');
+        /*$app['config']->set('database.default', 'test');
         $app['config']->set('database.connections.test', [
             'driver' => 'sqlite',
             'database' => ':memory:',
-        ]);
+        ]);*/
 
         $app['config']->set('auth.providers.users.model', User::class);
 
