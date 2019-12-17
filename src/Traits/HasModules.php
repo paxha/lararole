82283273a130aab2dbdb177a2c6424701904a436
Hassan Raza Pasha <e.hassanrazapasha@gmail.com>
2019-12-17 15:24:07 +0500

    Update HasModules.php

--- src/Traits/HasModules.php
+++ src/Traits/HasModules.php
@@ -9,10 +9,6 @@ trait HasModules
     public function assignModules(array $modules, array $permissions = null)
     {
         foreach ($modules as $index => $module) {
-            if (gettype($module) !== 'integer') {
-                return false;
-            }
-
             $module1 = Module::find($module);
             $this->modules()->attach($module1, ['permission' => @$permissions[$index] ? $permissions[$index] : 'read']);
         }
