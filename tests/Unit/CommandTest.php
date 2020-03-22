2f9fd8f30740c81fac5fab87173556cde529f7e9
Paxha <xpaxha@gmail.com>
2020-03-22 17:50:39 +0500

    testMigrateModulesCommand function position changed

--- tests/Unit/CommandTest.php
+++ tests/Unit/CommandTest.php
@@ -70,6 +70,26 @@ class CommandTest extends TestCase
         ],
     ];
 
+    public function testMakeViewsCommand()
+    {
+        $this->artisan('migrate:modules');
+        $this->artisan('make:views');
+
+        foreach ($this->moduleViews as $moduleView) {
+            $this->assertTrue(view()->exists($moduleView . '.create'));
+            $this->assertTrue(view()->exists($moduleView . '.edit'));
+            $this->assertTrue(view()->exists($moduleView . '.index'));
+            $this->assertTrue(view()->exists($moduleView . '.show'));
+        }
+
+        foreach ($this->excludeModuleViews as $excludeModuleView) {
+            $this->assertFalse(view()->exists($excludeModuleView . '.create'));
+            $this->assertFalse(view()->exists($excludeModuleView . '.edit'));
+            $this->assertFalse(view()->exists($excludeModuleView . '.index'));
+            $this->assertFalse(view()->exists($excludeModuleView . '.show'));
+        }
+    }
+
     public function testMigrateModulesCommand()
     {
         $this->artisan('migrate:modules');
@@ -91,26 +111,6 @@ class CommandTest extends TestCase
         $superAdminRole = Role::whereSlug('super-admin')->first();
 
         $this->assertCount(12, $superAdminRole->modules);
-    }
-
-    public function testMakeViewsCommand()
-    {
-        $this->artisan('migrate:modules');
-        $this->artisan('make:views');
-
-        foreach ($this->moduleViews as $moduleView) {
-            $this->assertTrue(view()->exists($moduleView . '.create'));
-            $this->assertTrue(view()->exists($moduleView . '.edit'));
-            $this->assertTrue(view()->exists($moduleView . '.index'));
-            $this->assertTrue(view()->exists($moduleView . '.show'));
-        }
-
-        foreach ($this->excludeModuleViews as $excludeModuleView) {
-            $this->assertFalse(view()->exists($excludeModuleView . '.create'));
-            $this->assertFalse(view()->exists($excludeModuleView . '.edit'));
-            $this->assertFalse(view()->exists($excludeModuleView . '.index'));
-            $this->assertFalse(view()->exists($excludeModuleView . '.show'));
-        }
     }
 
     public function testMakeSuperAdminRoleCommand()
