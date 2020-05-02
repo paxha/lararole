52fab5bb735b8bfbecaafe21e3d79ec8ef33ee78
Pasha <xpaxha@gmail.com>
2020-05-02 17:51:14 +0500

    web.php updated

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('lararole')->group(function () {
-    Route::get('/{any?}', 'HomeController@index')->where('any', '.*');
-});
+Route::get('/{any?}', 'HomeController@index')->where('any', '.*');
