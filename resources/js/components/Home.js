b1a98e16ba52f8f55765900f46bebdfaa9fff1fb
Pasha <xpaxha@gmail.com>
2020-05-11 19:33:19 +0500

    Dashboard Added
    ## Change log
    ### Components
    Home.js updated

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Breadcrumb, Layout } from 'antd'
-import { HomeOutlined } from '@ant-design/icons'
+import { Breadcrumb, Card, Col, Row, Statistic, Timeline } from 'antd'
+import { ArrowDownOutlined, ArrowUpOutlined, HomeOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
-
-const { Content } = Layout
 
 function Home () {
   return (
@@ -15,13 +13,94 @@ function Home () {
           </Link>
         </Breadcrumb.Item>
       </Breadcrumb>
-      <Content className="site-layout-background" style={{
-        padding: 24,
-        margin: 0,
-        minHeight: 280
-      }}>
-                Welcome To Lararole
-      </Content>
+      <Card title={'Modules'}>
+        <Row gutter={16}>
+          <Col span={4}>
+            <Statistic title="Total Modules" value={24}/>
+          </Col>
+          <Col span={4}>
+            <Statistic
+              title="Active"
+              value={24}
+              valueStyle={{ color: '#3f8600' }}
+              prefix={<ArrowUpOutlined/>}
+            />
+          </Col>
+          <Col span={4}>
+            <Statistic
+              title="Idle"
+              value={24}
+              valueStyle={{ color: '#cf1322' }}
+              prefix={<ArrowDownOutlined/>}
+            />
+          </Col>
+          <Col span={4}>
+            <Statistic title="Trashed" value={24} valueStyle={{ color: '#cf1322' }}/>
+          </Col>
+        </Row>
+      </Card>
+      <Card title={'Roles'} style={{ marginTop: 16 }}>
+        <Row gutter={16}>
+          <Col span={4}>
+            <Statistic title="Total Modules" value={24}/>
+          </Col>
+          <Col span={4}>
+            <Statistic title="Recent Added" value={24} valueStyle={{ color: '#3f8600' }}/>
+          </Col>
+          <Col span={4}>
+            <Statistic
+              title="Active"
+              value={24}
+              valueStyle={{ color: '#3f8600' }}
+              prefix={<ArrowUpOutlined/>}
+            />
+          </Col>
+          <Col span={4}>
+            <Statistic
+              title="Idle"
+              value={24}
+              valueStyle={{ color: '#cf1322' }}
+              prefix={<ArrowDownOutlined/>}
+            />
+          </Col>
+          <Col span={4}>
+            <Statistic title="Trashed" value={24} valueStyle={{ color: '#cf1322' }}/>
+          </Col>
+        </Row>
+      </Card>
+
+      <Row gutter={16} style={{ marginTop: 16 }}>
+        <Col span={12}>
+          <Card
+            title="Recent Activities"
+          >
+            <Timeline>
+              <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+              <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
+              <Timeline.Item color="red">
+                <p>Solve initial network problems 1</p>
+                <p>Solve initial network problems 2</p>
+                <p>Solve initial network problems 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item>
+                <p>Technical testing 1</p>
+                <p>Technical testing 2</p>
+                <p>Technical testing 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item color="gray">
+                <p>Technical testing 1</p>
+                <p>Technical testing 2</p>
+                <p>Technical testing 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item color="gray">
+                <p>Technical testing 1</p>
+                <p>Technical testing 2</p>
+                <p>Technical testing 3 2015-09-01</p>
+              </Timeline.Item>
+            </Timeline>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
